8d36dee748324ff5c63ac210bc1bd6ba8e6f8213
RISTHER-67 <emiliocanepa08@gmail.com>
2026-05-10 00:57:13 -0500

    logo de pagina y palabras claves

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,13 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="description" content="Dulces Regionales Doña Solina">
+    <meta name="keywords" content="Dulces Regionales Doña Solina,Dulces Regionales,Dulces">
+    <meta name="author" content="Dulces Regionales Doña Solina-RisterTech">
     <title>Dulces Regionales Doña Solina</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="shortcut icon" href="/dulces_regionales/imagenes/logo.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/dulces_regionales/assets/css/global.css">
-    <?php 
+    <?php
     $viewController = isset($_GET['controller']) ? $_GET['controller'] : 'auth';
     $cssFile = "/dulces_regionales/assets/css/views/{$viewController}.css";
     ?>
